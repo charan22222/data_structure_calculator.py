2ea1751238d60b0ba0348899e29e2b79fba83623
charan22222 <77820282+charan22222@users.noreply.github.com>
2021-07-12 10:38:50 +0530

    Create Datatypes_calculator.py

---
+++ Datatypes_calculator.py
@@ -0,0 +1,19 @@
+#data types
+# 1. list
+
+my_list = ["apple","banana","kiwi"]
+print(my_list)
+
+my_list.append("orange")
+print(my_list)
+
+my_list.remove("orange")
+print(my_list)
+
+tropical = ["mango","papaya"]
+my_list.extend(tropical)
+print(my_list)
+
+for x in my_list:
+  print(x)
+
